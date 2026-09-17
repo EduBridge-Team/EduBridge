fef3517e063258204b3ea373f001c47edec3f22d
Mohammed Emad <126204744+MohammedEmad333@users.noreply.github.com>
2026-09-17 03:06:19 +0300

    security(api): register child access middleware

--- edubridge-api-laravel/bootstrap/app.php
+++ edubridge-api-laravel/bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\JwtAuth;
 use App\Http\Middleware\RoleMiddleware;
+use App\Http\Middleware\ChildAccessMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -20,6 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'auth.jwt' => JwtAuth::class,
             'role' => RoleMiddleware::class,
+            'child.access' => ChildAccessMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
