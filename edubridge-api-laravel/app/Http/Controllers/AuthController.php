1176d63648aaa40da142eaccac4abeee7c2008e1
Mohammed Emad <126204744+MohammedEmad333@users.noreply.github.com>
2026-09-24 05:09:18 +0300

    refactor(api): compose split credential actions

--- edubridge-api-laravel/app/Http/Controllers/AuthController.php
+++ edubridge-api-laravel/app/Http/Controllers/AuthController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Concerns\AuthControllerHelpers;
-use App\Http\Controllers\Concerns\AuthCredentialActions;
 use App\Http\Controllers\Concerns\AuthGoogleActions;
+use App\Http\Controllers\Concerns\AuthLoginActions;
+use App\Http\Controllers\Concerns\AuthRegisterActions;
 
 class AuthController extends Controller
 {
     use AuthControllerHelpers;
-    use AuthCredentialActions;
+    use AuthRegisterActions;
+    use AuthLoginActions;
     use AuthGoogleActions;
 }
