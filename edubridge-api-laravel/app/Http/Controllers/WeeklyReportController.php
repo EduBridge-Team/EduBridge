5d629b9c09ebe8d0160a28a9bdc6d7232c9cd2b2
Mohammed Emad <126204744+MohammedEmad333@users.noreply.github.com>
2026-09-24 05:37:15 +0300

    fix(api): import weekly report specialist helper

--- edubridge-api-laravel/app/Http/Controllers/WeeklyReportController.php
+++ edubridge-api-laravel/app/Http/Controllers/WeeklyReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Concerns\WeeklyReportControllerHelpers;
 use App\Http\Controllers\Concerns\WeeklyReportGeneralWriteActions;
 use App\Http\Controllers\Concerns\WeeklyReportReadActions;
+use App\Http\Controllers\Concerns\WeeklyReportSpecialistHelpers;
 use App\Http\Controllers\Concerns\WeeklyReportSpecialistWriteActions;
 
 class WeeklyReportController extends Controller
