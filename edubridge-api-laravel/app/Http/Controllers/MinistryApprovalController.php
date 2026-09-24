109d8d0c677fb9c754bdec80f85e253b105cf22e
Mohammed Emad <126204744+MohammedEmad333@users.noreply.github.com>
2026-09-24 05:19:24 +0300

    refactor(api): compose split ministry approval writes

--- edubridge-api-laravel/app/Http/Controllers/MinistryApprovalController.php
+++ edubridge-api-laravel/app/Http/Controllers/MinistryApprovalController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Concerns\MinistryApprovalCreateActions;
 use App\Http\Controllers\Concerns\MinistryApprovalHelpers;
 use App\Http\Controllers\Concerns\MinistryApprovalReadActions;
-use App\Http\Controllers\Concerns\MinistryApprovalWriteActions;
+use App\Http\Controllers\Concerns\MinistryApprovalReviewActions;
 
 class MinistryApprovalController extends Controller
 {
     use MinistryApprovalHelpers;
     use MinistryApprovalReadActions;
-    use MinistryApprovalWriteActions;
+    use MinistryApprovalCreateActions;
+    use MinistryApprovalReviewActions;
 }
