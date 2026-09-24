ac15ec26f09b40a05cf936a72b328aa089bc75df
Mohammed Emad <126204744+MohammedEmad333@users.noreply.github.com>
2026-09-24 05:14:38 +0300

    refactor(api): compose smaller consultation write traits

--- edubridge-api-laravel/app/Http/Controllers/ConsultationController.php
+++ edubridge-api-laravel/app/Http/Controllers/ConsultationController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Concerns\ConsultationAccess;
+use App\Http\Controllers\Concerns\ConsultationCreateActions;
+use App\Http\Controllers\Concerns\ConsultationNoteActions;
 use App\Http\Controllers\Concerns\ConsultationReadActions;
-use App\Http\Controllers\Concerns\ConsultationWriteActions;
+use App\Http\Controllers\Concerns\ConsultationUpdateActions;
 
 class ConsultationController extends Controller
 {
     use ConsultationAccess;
     use ConsultationReadActions;
-    use ConsultationWriteActions;
+    use ConsultationCreateActions;
+    use ConsultationUpdateActions;
+    use ConsultationNoteActions;
 }
