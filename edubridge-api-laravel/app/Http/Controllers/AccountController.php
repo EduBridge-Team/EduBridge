d0cc90f71cc4a1a53effa51f1148b677e6dec410
Mohammed Emad <126204744+MohammedEmad333@users.noreply.github.com>
2026-09-24 05:23:32 +0300

    refactor(api): compose split account profile actions

--- edubridge-api-laravel/app/Http/Controllers/AccountController.php
+++ edubridge-api-laravel/app/Http/Controllers/AccountController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Concerns\AccountAvatarActions;
 use App\Http\Controllers\Concerns\AccountControllerHelpers;
-use App\Http\Controllers\Concerns\AccountProfileActions;
+use App\Http\Controllers\Concerns\AccountDeleteActions;
+use App\Http\Controllers\Concerns\AccountPasswordActions;
+use App\Http\Controllers\Concerns\AccountProfileReadActions;
 
 class AccountController extends Controller
 {
     use AccountControllerHelpers;
     use AccountAvatarActions;
-    use AccountProfileActions;
+    use AccountProfileReadActions;
+    use AccountPasswordActions;
+    use AccountDeleteActions;
 
     private const MIME_EXTENSIONS = [
         'image/jpeg' => 'jpg',
