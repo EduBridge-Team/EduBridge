d0db3f4d54c8b92e31ed23fbcb5cc752d5348e5e
Mohammed Emad <126204744+MohammedEmad333@users.noreply.github.com>
2026-09-24 05:07:06 +0300

    refactor(api): compose smaller session write traits

--- edubridge-api-laravel/app/Http/Controllers/SessionController.php
+++ edubridge-api-laravel/app/Http/Controllers/SessionController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Concerns\SessionCompleteActions;
 use App\Http\Controllers\Concerns\SessionControllerHelpers;
+use App\Http\Controllers\Concerns\SessionCreateActions;
 use App\Http\Controllers\Concerns\SessionReadActions;
-use App\Http\Controllers\Concerns\SessionWriteActions;
+use App\Http\Controllers\Concerns\SessionUpdateActions;
 
 class SessionController extends Controller
 {
     use SessionControllerHelpers;
     use SessionReadActions;
-    use SessionWriteActions;
+    use SessionCreateActions;
+    use SessionCompleteActions;
+    use SessionUpdateActions;
 
     private const DB_STATUSES = ['scheduled', 'done', 'cancelled'];
     private const TYPES = ['learningPlanning', 'followUp', 'parentReview', 'teamReview', 'groupSupport'];
