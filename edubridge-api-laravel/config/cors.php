1acfb61abb090c394188252141507003491e3836
Mohammed Emad <126204744+MohammedEmad333@users.noreply.github.com>
2026-09-26 16:14:52 +0300

    fix(api): allow localhost origins for web development

--- edubridge-api-laravel/config/cors.php
+++ edubridge-api-laravel/config/cors.php
@@ -7,7 +7,10 @@ return [
         'https://edubridge.win',
         'https://www.edubridge.win',
     ],
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^http://localhost:[0-9]+$#',
+        '#^http://127[.]0[.]0[.]1:[0-9]+$#',
+    ],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
